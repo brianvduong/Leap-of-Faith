40ff04142238b16d57d3c38dda019673ff80e029
Brian Duong <brianvduong@gmail.com>
2020-04-09 15:49:16 -0700

    added player models, ground, and platform

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float moveSpeed;
+
+    private Rigidbody2D rb;
+    private bool facingRight = true;
+    private float moveDirection;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //get inputs
+        moveDirection = Input.GetAxis("Horizontal"); //scale of -1 to 1
+
+        //move
+        rb.velocity = new Vector2(moveDirection * moveSpeed, rb.velocity.y);
+    }
+}
